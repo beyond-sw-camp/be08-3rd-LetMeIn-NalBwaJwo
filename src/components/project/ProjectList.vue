9bd1e45bc79fb1d0d7dffd5b2e4a07499c94856d
DESKTOP-1MRFVUA\Playdata <jiyeonjeon01@naver.com>
2024-08-17 16:36:28 +0900

    projectlist 추가

---
+++ src/components/project/ProjectList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="project-list">
+      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+    </div>
+  </template>
+  
+  <script>
+  import ProjectCard from './ProjectCard.vue'
+  
+  export default {
+    components: {
+      ProjectCard
+    },
+    props: {
+      projects: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* 스타일 설정 */
+  </style>
+  
