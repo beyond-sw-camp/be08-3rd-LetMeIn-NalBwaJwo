63b4d07660f0740254df3b3cab5e6d3eaf82af79
yongun2 <djkye0001@naver.com>
2024-08-18 18:34:34 +0900

    이력세 세부사항 폼 컴포넌트

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,17 @@
 import MainNavHeader from "./common/MainNavHeader.vue";
-
+import WorkExperienceForm from "./resume/WorkExperienceForm.vue";
+import AwardsAndCertificationForm from "./resume/AwardsAndCertificationForm.vue";
+import ResumeFormLayout from "./layout/resume/ResumeFormLayout.vue";
+import EducationForm from "./resume/EducationForm.vue";
+import ResumeDetailHeader from "./resume/ResumeDetailHeader.vue";
+// common
 export { MainNavHeader };
+
+// resume
+export {
+  ResumeDetailHeader,
+  ResumeFormLayout,
+  WorkExperienceForm,
+  AwardsAndCertificationForm,
+  EducationForm,
+};
