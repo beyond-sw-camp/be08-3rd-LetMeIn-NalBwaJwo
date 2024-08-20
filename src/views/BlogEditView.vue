24cddafcbac88a329effe883667e23160332ba62
henhen7 <eatho@naver.com>
2024-08-20 20:07:54 +0900

    포트폴리오 여백 수정, 내용 수정

---
+++ src/views/BlogEditView.vue
@@ -0,0 +1,11 @@
+<template>
+    <MdEditor language="en-US" v-model="text" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { MdEditor } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('Hello Editor!');
+</script>
