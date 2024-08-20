5b74531e9c24655534ac0f23cf00e4b29fdac23b
jason-gb <a01071865081@gmail.com>
2024-08-20 20:39:21 +0900

    선택 부분 별 표시 진행

--- src/components/resume/ResumeList.vue
+++ src/components/resume/ResumeList.vue
@@ -1,6 +1,6 @@
 <template>
   <hr class="first-hr" />
-  <div v-for="(list, i) in data" :key="i" class="resume">
+  <div v-for="(list, index) in data" :key="index" class="resume">
     <div class="info">
       <div class="icons-wrapper">
         <EditIcon class="icon" />
@@ -8,7 +8,10 @@
         <CopyIcon class="icon" />
         <DeleteIcon class="icon" />
       </div>
-      <h3 class="text-bold">{{ list.title }}</h3>
+      <h3 @click="selectItem(index)">{{ list.title }}
+        <!-- 조건에 따라 다른 컴포넌트 렌더링 -->
+          <StarIcon v-if="selectedIndex === index" />
+      </h3>
       <p>{{ list.date }}</p>
       <hr class="last-hr" />
     </div>
@@ -21,29 +24,36 @@ import MaterialSymbolsLightEditOutline from "~icons/material-symbols-light/edit-
 import MaterialSymbolsLightPageviewOutline from "~icons/material-symbols-light/pageview-outline";
 import MaterialSymbolsLightFileCopyOutlineRounded from "~icons/material-symbols-light/file-copy-outline-rounded";
 import MaterialSymbolsLightDeleteOutline from "~icons/material-symbols-light/delete-outline";
+import MeteoconsStarFill from '~icons/meteocons/star-fill';
+import MaterialSymbolsLightStarOutline from '~icons/material-symbols-light/star-outline';
+
 export default {
   name: "ResumeList",
   data() {
     return {
+      selectedIndex: null, // 선택 이력서 인덱스
       data: [
         {
-          title: "hello",
+          index: 1,
+          title: "일등",
           date: "2023-03-23",
         },
         {
-          title: "hello",
+          index: 2,
+          title: "이등",
           date: "2023-03-23",
         },
         {
-          title: "hello",
+          index: 3,
+          title: "삼등",
           date: "2023-03-23",
         },
       ],
     };
   },
   methods: {
-    increseLike(i) {
-      this.data[i].like += 1;
+    selectItem(index) {
+      this.selectedIndex = index; // 선택된 인덱스를 업데이트
     },
   },
   components: {
@@ -51,6 +61,16 @@ export default {
     PageViewIcon: MaterialSymbolsLightPageviewOutline,
     CopyIcon: MaterialSymbolsLightFileCopyOutlineRounded,
     DeleteIcon: MaterialSymbolsLightDeleteOutline,
+    StarIcon: MeteoconsStarFill,
+    HallStarIcon: MaterialSymbolsLightStarOutline,
+  },
+  computed: {
+    selectedItem() {
+      // 선택된 항목을 반환
+      return this.selectedIndex !== null
+        ? this.items[this.selectedIndex]
+        : null;
+    },
   },
 };
 </script>
